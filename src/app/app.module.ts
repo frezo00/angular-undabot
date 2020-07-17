1d0f7e310a51e6e6be918f7592fdc918f9ee8c15
Frano Rezo <frezo@obi4wan.com>
2020-07-18 00:10:59 +0200

    03-Task: Custom component for form errors display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
 import { ContactComponent } from './components/contact/contact.component';
+import { FormControlErrorComponent } from './components/form-control-error/form-control-error.component';
 import { HomeComponent } from './components/home/home.component';
 import { SafePipe } from './pipes/safe.pipe';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ContactComponent, SafePipe],
+  declarations: [AppComponent, HomeComponent, ContactComponent, FormControlErrorComponent, SafePipe],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
